4deca8b32decca231ca57ddc1947c98131302396
Conner-Stack <connerstack@gmail.com>
2017-03-22 16:15:16 -0500

    push
    progress(?)

--- Python Projects/aStarAlgorithm.py
+++ Python Projects/aStarAlgorithm.py
@@ -5,13 +5,14 @@ import drawablenode
 from graph import Graph
 from graph import Node
 
+
 class Pathfinding(object):
     
-def aStar(start, goal)
-closedNodes = 
-openNodes = {start}
-From = graph.Graph
-gScore = math.inf
-gScore[Start] = 0
-fscore = graph.Graph
+ #   def aStar(start, goal):
+ #       Dequeued = {}
+ #       Queued = {start}
+ #       From = graph.Graph
+ #       gScore = math.inf
+ #       gScore[Start] = 
+ #       fscore = graph.Graph
 
