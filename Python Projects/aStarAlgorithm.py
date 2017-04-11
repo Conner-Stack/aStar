ef39f190e174b414fab7b4da46af871c4e5a1bc5
Conner-Stack <connerstack@gmail.com>
2017-04-11 17:10:33 -0500

    push
    idk what to do about it anymore, throwing crap at the wall to see what
    sticks

--- Python Projects/aStarAlgorithm.py
+++ Python Projects/aStarAlgorithm.py
@@ -6,13 +6,15 @@ from graph import Graph
 from graph import Node
 
 class pathFinder(object):
-    
-                   Start = [posx , posy]
-           Openqueue = []
-           Closequeue = []
-           Openqueue.append(Start)
-           
+    '''pathfinder'''
+    Start = [posx , posy]
+    Openqueue = []
+    Closequeue = []
+    Openqueue.append(Start)
+
         def aStart(Start, Goal, graph):
+            Start = Graph.
+            
 
         #the append and insert functions for lists will be how I add to my queue
         #remove will be a function i use to dequeue my nodes        
