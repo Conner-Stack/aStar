b152b43a606985fa69e95452650050af8b193961
Conner-Stack <connerstack@gmail.com>
2017-04-11 17:54:27 -0500

    end of day push
    finally was able to get started

--- Python Projects/aStarAlgorithm.py
+++ Python Projects/aStarAlgorithm.py
@@ -8,13 +8,23 @@ from graph import Node
 class pathFinder(object):
     '''pathfinder'''
 
-    Openqueue = []
-    Closequeue = []
+ 
     
 
-        def aStart(Start, Goal, graph):
-        Start =         
-        Openqueue.append(Start)        
+    def aStart(Start, Goal, graph):
+            Openqueue = []
+            Closequeue = []
+            Openqueue.append(Start)
+            current = Start
+            Openqueue.append(current)  
+            while current!=Goal:
+                #make current equal a child node
+                current = 
+                Closequeue.append(current)
+
+                    
+                      
+
 
         #the append and insert functions for lists will be how I add to my queue
         #remove will be a function i use to dequeue my nodes        
