ede10677187de851d17dfdcfbd87fe36ac494c16
Conner-Stack <connerstack@gmail.com>
2017-04-17 12:12:11 -0500

    push
    errors everywhere :/

--- Python Projects/aStarAlgorithm.py
+++ Python Projects/aStarAlgorithm.py
@@ -5,39 +5,25 @@ import drawablenode
 from graph import Graph
 from graph import Node
 from drawablenode import DrawableNode
-from graph import get_neighbors
 class pathFinder(object):
-    '''pathfinder'''
-
- 
-    
-#create function to set your start and end goal
+    '''pathfinder'''  
+    #create function to set your start and end goal
     def aStart(self, Start, Goal):
-          
             # create a list to store children
-            Open = []
+        Openlist = []
             #create a list to store parents
-            Close = []
+        Closelist = []
             #make a list for children
-            children = []
-            fCost = 0
-            gScore = 0
-            hScore = 0
-            #place Start in the open list 
-            Open.append(Start)
-            gscore.Start = 0
-
-            # forms a loop iterating through each node adjacent to the current node
-              for neighbor in get_neighbors(current):
-                  # if it's not in the closed list
-                if (NotClose == True):
-                while(NotClose = True):
-                  if Walkable == True:
-                      Open.append(neighbor)
-                      Open.
-                      
-                  #not right, but kind of a placeholder until i figure out how to do it
-               
-
-                  
-                  
+        Children = []
+        fCost = 0
+        gScore = 0
+        hScore = 0
+        #place Start in the open list 
+        Openlist.append(Start)
+        while len(Openlist) > 0:
+            lowest = Openlist[0]
+            for x in Openlist:
+                if  x.fCost < lowest.fCost:
+                    lowest = x
+                    Openlist.remove(lowest)
+                    Closelist.append(lowest)
