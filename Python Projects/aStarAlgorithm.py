6dcd83ddc6d3671de852d332d0552f34b52dbef3
Conner-Stack <connerstack@gmail.com>
2017-04-17 10:48:46 -0500

    push
    progress

--- Python Projects/aStarAlgorithm.py
+++ Python Projects/aStarAlgorithm.py
@@ -12,25 +12,29 @@ class pathFinder(object):
  
     
 #create function to set your start and end goal
-    def aStart(Start, Goal, graph):
+    def aStart(self, Start, Goal):
           
             # create a list to store children
             Open = []
             #create a list to store parents
             Close = []
-            #place Start in the open queue
+            #make a list for children
+            children = []
+            fCost = 0
+            gScore = 0
+            hScore = 0
+            #place Start in the open list 
             Open.append(Start)
-            
-            current = Start
+            gscore.Start = 0
+
+            # forms a loop iterating through each node adjacent to the current node
               for neighbor in get_neighbors(current):
-                NotClose = True
+                  # if it's not in the closed list
+                if (NotClose == True):
                 while(NotClose = True):
-                    
                   if Walkable == True:
-                      
-                          
                       Open.append(neighbor)
-                      
+                      Open.
                       
                   #not right, but kind of a placeholder until i figure out how to do it
                
